b30212aecf5d81b8e738ced0496e0534c7f7365d
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-05-05 14:27:35 -0400

    Initial Commit

---
+++ Challenge7/Challenge7App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge7App.swift
+//  Challenge7
+//
+//  Created by Fahim Uddin on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
